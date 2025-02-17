f3447a53fd8334341af5e93fe7154597b0273451
Gergely Rozsas <rozsas.greg1@gmail.com>
2025-02-17 19:50:09 +0100

    masodik

--- sneakers.py
+++ sneakers.py
@@ -4,13 +4,12 @@ import pprint
 def beolvas_csv(fajlnev):
     with open(fajlnev, encoding='utf-8') as fajl:
         sorok = fajl.read().splitlines()
-    fejlec = sorok[0].split(",")  # Első sor: oszlopnevek
-    adatok = [sor.split(",") for sor in sorok[0:]]  # Adatok feldolgozása listába
+    fejlec = sorok[0].split(",")
+    adatok = [sor.split(",") for sor in sorok[0:]]
     return fejlec, adatok
 
 
 def kiir_rendezesi_lehetosegek():
-    """ Kiírja a választható rendezési szempontokat """
     lehetosegek = {
         "1": "title",
         "2": "color_breif",
@@ -24,7 +23,7 @@ def kiir_rendezesi_lehetosegek():
     return lehetosegek
 
 
-def kivalaszt_rendezesi_szempont(lehetosegek):
+def rendezesi_szempont(lehetosegek):
     valasztas = input("Add meg a lehetőség számát! ").strip()
     if valasztas not in lehetosegek:
         print("Érvénytelen választás! Kérlek, egy számot adj meg a listából.")
@@ -45,7 +44,7 @@ def main():
     csv_fajl = "sneakers.csv"
     fejlec, cipok = beolvas_csv(csv_fajl)
     lehetosegek = kiir_rendezesi_lehetosegek()
-    oszlop_nev = kivalaszt_rendezesi_szempont(lehetosegek)
+    oszlop_nev = rendezesi_szempont(lehetosegek)
     rendezett_cipok = rendez_adatok(fejlec, cipok, oszlop_nev)
 
     for cipo in rendezett_cipok:
